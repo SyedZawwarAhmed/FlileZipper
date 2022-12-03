1ff2658d681a3da602d5aa64ff76fa3a5a59af7c
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-12-03 12:43:33 +0500

    update getFrequencies.cpp

--- Encoding/getFrequencies.cpp
+++ Encoding/getFrequencies.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+// #include "Character.h"
 
 using namespace std;
 
-struct Character {
-    char character;
-    int frequency;
-};
-
-Character *getFrequencies(char charArray[]) {
-    
+Character *getFrequencies(char charArray[])
+{
     size_t len = strlen(charArray);
-    cout << charArray << endl;
+    Character *frequenciesArray = new Character[256];
+    for (int i=0; i < 256; i++) {
+        frequenciesArray[i].character = char(i);
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        int index = int(charArray[i]);
+        // frequenciesArray[index].character = charArray[i];
+        frequenciesArray[index].frequency++;
+    }
+    return frequenciesArray;
 }
