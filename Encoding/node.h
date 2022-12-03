ceb68fadaf02cab09f0e6df8eab59c243ffdb974
Annas03 <baigannas9@gmail.com>
2022-12-03 11:32:16 +0500

    feat: create node.h

---
+++ Encoding/node.h
@@ -0,0 +1,19 @@
+class node{
+    public:
+        int val;
+        node *left_child;
+        node *right_child;
+
+        node(int val){
+            this->val = val;
+            this->left_child = NULL;
+            this->right_child = NULL;
+        }
+        node(){
+            this->left_child = NULL;
+            this->right_child = NULL;
+        }
+        // ~node(){
+        //     delete left_child, right_child;
+        // }
+};
