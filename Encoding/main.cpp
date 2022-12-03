22d87dcc04e917ed9bcdb1f1fe0e195aa478f8b9
Annas03 <baigannas9@gmail.com>
2022-12-03 11:32:07 +0500

    update main.cpp

--- Encoding/main.cpp
+++ Encoding/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
 using namespace std;
 
+char* hauffmanTree(){
+    // while(given_array.lenght != 1){
 
+        // find two minimum frequencies
+        // create a binary tree node and put the two minimum in left and right child
+        // Add this tree node to given_array and remove the two minimum from given_array
+    // }
+}
+int findMin(int* arr){
+
+}
 
 int main() {
     return 0;
