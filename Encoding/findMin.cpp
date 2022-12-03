f11a22f5ef843d3e67fcb077a481db5bb018faf9
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-12-03 15:40:41 +0500

    update findMin.cpp

--- Encoding/findMin.cpp
+++ Encoding/findMin.cpp
@@ -1,13 +1,15 @@
 #include <limits>
 
-int findMin(Character *frequenciesArray)
+Character findMin(Character *frequenciesArray)
 {
     int minimumFrequency = std::numeric_limits<int>::max();
     int index;
     for (int i = 0; i<256; i++) {
         if (frequenciesArray[i].frequency < minimumFrequency && frequenciesArray[i].frequency > 0) {
             minimumFrequency = frequenciesArray[i].frequency;
+            index = i;
         }
     }
-    return minimumFrequency;
+    Character minimumFrequencyCharacter = frequenciesArray[index];
+    return minimumFrequencyCharacter;
 }
