3142498580642d0f0e575194d4027b71424a509e
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-12-03 15:40:51 +0500

    update main.cpp

--- Encoding/main.cpp
+++ Encoding/main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 int main()
 {
-    char fileName[] = "inputFile.txt";
+    char fileName[] = "../inputFile.txt";
     char *charArray = readFile(fileName);
     Character *frequenciesArray = getFrequencies(charArray);
-    for (int i =0; i<256; i++) 
-    if (frequenciesArray[i].frequency > 0) cout << frequenciesArray[i].character << "    " << frequenciesArray[i].frequency << endl;
-    cout << findMin(frequenciesArray) << endl;
+    for (int i = 0; i < 13; i++)
+        cout << frequenciesArray[i].character << "    " << frequenciesArray[i].frequency << endl;
+    cout << findMin(frequenciesArray).character << "   " << findMin(frequenciesArray).frequency << endl;
     return 0;
 }
