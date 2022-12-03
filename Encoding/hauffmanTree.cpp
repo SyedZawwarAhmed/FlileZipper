c4f5781fd8ef6e532bbf919524483f60490843de
Annas03 <baigannas9@gmail.com>
2022-12-03 15:44:04 +0500

    update hauffmanTree.cpp

--- Encoding/hauffmanTree.cpp
+++ Encoding/hauffmanTree.cpp
@@ -1,10 +1,26 @@
-char *hauffmanTree()
-{
-    // while(given_array.lenght != 1){
+#include "Character.h"
 
-    // find two minimum frequencies
-    // create a binary tree node and put the two minimum in left and right child
-    // Add this tree node to given_array and remove the two minimum from given_array
-    // }
-    
+BinarySearchTreeNode* hauffmanTree(Character* frequenciesArray){
+    Character* temp_frequenciesArray = frequenciesArray;
+    while(temp_frequenciesArray != 1){
+
+        // create a binary tree BinarySearchTreeNode and put the two minimum in left and right child
+        BinarySearchTreeNode* root_Node;
+
+        Character min_struct, root;
+        // find two minimum frequencies
+        min_struct = findMin(temp_frequenciesArray);
+        root_Node->left_child = min_struct.character;
+        root_Node->val = min_struct.frequency;
+        // delete min_struct from temp_frequenciesArray;
+
+        min_struct = findMin(temp_frequenciesArray);
+        root_Node->right_child = findMin(temp_frequenciesArray);
+        root_Node->val += min_struct.frequency;
+        // delete min_struct from temp_frequenciesArray;
+
+        // Add this tree BinarySearchTreeNode to given_array
+        root.frequency = root_Node.val;
+        temp_frequenciesArray.insert(root_Node);
+    }
 }
