4c666f756b63c155df5a6470f8c430d2f16763b1
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-12-03 12:08:40 +0500

    feat: create getFrequencies.cpp

---
+++ Encoding/getFrequencies.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+struct Character {
+    char character;
+    int frequency;
+};
+
+Character *getFrequencies(char charArray[]) {
+    
+    size_t len = strlen(charArray);
+    cout << charArray << endl;
+}
