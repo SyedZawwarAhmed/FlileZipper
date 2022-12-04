13e227f8f9885f936e310f6a87b31f0d7b580e38
Annas03 <baigannas9@gmail.com>
2022-12-04 11:49:25 +0500

    update getFrequencies.cpp

--- Encoding/getFrequencies.cpp
+++ Encoding/getFrequencies.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-Character *getFrequencies(char charArray[])
+vector<BinarySearchTreeNode> getFrequencies(char charArray[])
 {
     size_t len = strlen(charArray);
-    Character *frequenciesArray = new Character[256];
+    vector<BinarySearchTreeNode> frequenciesArray(256);
     for (int i = 0; i < 256; i++)
     {
         frequenciesArray[i].character = char(i);
@@ -25,11 +25,10 @@ Character *getFrequencies(char charArray[])
             filteredFrequenciesArraySize++;
 
     int filteredFrequenciesArrayIndex = 0;
-    Character* filteredFrequenciesArray = new Character[filteredFrequenciesArraySize];
+    vector<BinarySearchTreeNode> filteredFrequenciesArray(filteredFrequenciesArraySize);
     for (int i = 0; i < 256; i++)
         if (frequenciesArray[i].frequency > 0)
             filteredFrequenciesArray[filteredFrequenciesArrayIndex++] = frequenciesArray[i];
 
-    delete frequenciesArray;
     return filteredFrequenciesArray;
 }
