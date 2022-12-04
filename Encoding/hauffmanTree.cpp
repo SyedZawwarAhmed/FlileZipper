4894d94de3c4764aa497df9d80dfddba5bd46aba
Annas03 <baigannas9@gmail.com>
2022-12-04 11:49:30 +0500

    update hauffmanTree.cpp

--- Encoding/hauffmanTree.cpp
+++ Encoding/hauffmanTree.cpp
@@ -1,29 +1,35 @@
-#include "Character.h"
-
-BinarySearchTreeNode* hauffmanTree(Character* frequenciesArray){
-    Character* temp_frequenciesArray = frequenciesArray;
-    while(temp_frequenciesArray != 1){
+BinarySearchTreeNode* hauffmanTree(const vector<BinarySearchTreeNode> frequenciesArray){
+    vector<BinarySearchTreeNode*> temp_frequencies;
+    for(int i=0; i<frequenciesArray.size(); i++){
+        BinarySearchTreeNode* temp_frequency_node = new BinarySearchTreeNode();
+        temp_frequency_node->character = frequenciesArray[i].character;
+        temp_frequency_node->frequency = frequenciesArray[i].frequency;
+        temp_frequencies.push_back(temp_frequency_node);
+    }
+    while(temp_frequencies.size() != 1){
 
         // create a binary tree BinarySearchTreeNode and put the two minimum in left and right child
-        BinarySearchTreeNode* root_Node;
 
         int minNumIndex;
+        BinarySearchTreeNode* temp_Node = new BinarySearchTreeNode();
 
-        Character root;
         // find two minimum frequencies
-        minNumIndex = findMin(temp_frequenciesArray);
-        root_Node->left_child = frequenciesArray[minNumIndex].character;
-        root_Node->val = min_struct.frequency;
-        // delete min_struct from temp_frequenciesArray;
-
-        minNumIndex = findMin(temp_frequenciesArray);
-        root_Node->right_child = frequenciesArray[minNumIndex].character;
-        root_Node->val += min_struct.frequency;
-        // delete min_struct from temp_frequenciesArray;
+        for(int i=0; i<=1; i++){
+            minNumIndex = getMinIndex(temp_frequencies);
+            if(i==0){
+                temp_Node->left_child = temp_frequencies[minNumIndex];
+            }
+            else{
+                temp_Node->right_child = temp_frequencies[minNumIndex];
+            }
+            temp_Node->frequency += temp_frequencies[minNumIndex]->frequency;
+            // delete min_struct from frequenciesArray;
+            temp_frequencies.erase(temp_frequencies.begin()+minNumIndex);
+        }
+        
 
         // Add this tree BinarySearchTreeNode to given_array
-        root.frequency = root_Node.val;
-        root.n = root_Node;
-        temp_frequenciesArray.insert(root_Node);
+        temp_frequencies.push_back(temp_Node);
     }
+    return temp_frequencies[0];
 }
