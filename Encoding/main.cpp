2c99d1b91ecad444d886d7669e6b0cceb1d9e3e8
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-12-04 16:11:52 +0500

    update main.cpp

--- Encoding/main.cpp
+++ Encoding/main.cpp
@@ -14,26 +14,29 @@ int main()
 {
     char fileName[] = "../inputFile.txt";
     char *charArray = readFile(fileName);
-    
+
     vector<BinarySearchTreeNode> frequenciesArray = getFrequencies(charArray);
 
     // for (int i = 0; i < 3; i++)
     //     cout << frequenciesArray[i].character << "    " << frequenciesArray[i].frequency << endl;
 
     // cout << frequenciesArray[getMinIndex(frequenciesArray)].character << "   " << frequenciesArray[getMinIndex(frequenciesArray)].frequency << endl;
-    BinarySearchTreeNode* root_Node = hauffmanTree(frequenciesArray);
+    BinarySearchTreeNode *root_Node = hauffmanTree(frequenciesArray);
     EncodedNode enc_node;
-    enc_node.character = root_Node->character; 
-    vector<char> weight = {'0'};
+    enc_node.character = root_Node->character;
+    vector<char> weight = {};
     enc_node.weight = weight;
     vector<EncodedNode> encoded_Node_Vector = {enc_node};
-    encode(root_Node,encoded_Node_Vector);
-    // for(int i =0; i<encoded_Node_Vector.size(); i++){
-    //     cout<<encoded_Node_Vector[i].character<<"  ";
-    //     for(int j=0; j<encoded_Node_Vector[i].weight.size(); j++){
-    //         cout<<encoded_Node_Vector[i].weight[j];
-    //     }
-    //     cout<<endl;
-    // }
+    // vector<EncodedNode> result = encode(root_Node, weight);
+    encode(root_Node, weight);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i].character << "  ";
+        for (int j = 0; j < result[i].weight.size(); j++)
+        {
+            cout << result[i].weight[j];
+        }
+        cout << endl;
+    }
     return 0;
 }
