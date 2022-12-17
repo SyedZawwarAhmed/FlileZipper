cbb7b508225fd23c464d96a2d68a5849a832450a
Annas03 <baigannas9@gmail.com>
2022-12-17 18:52:28 +0500

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
-
+#include <string>
+#include <fstream>
+#include <bitset>
 using namespace std;
 
-int add(int a,int b) {
-    return a + b;
-}
-
-int subtract (int a,int b) {
-    return a-b;
-}
-
-int main() {
-    cout << add(2, 3) << endl;
+// gives the number of bytes we need to store in binary file
+int multiple(string encoded_string){
+    int added_bits = 0;
+    if(encoded_string.length()%8 != 0){
+        added_bits = 8 - encoded_string.length()%8;
+    }
+    for(int i=1; i<100; i++){
+        if(8*i == encoded_string.length() + added_bits){
+            return i;
+        }
+    }
     return 0;
-};
+}
+
+void getEncodedBinaryFile(string encoded_string){
+    int m = multiple(encoded_string);
+    ofstream myfile;
+    myfile.open("D:\\Annas Docs\\fourth sem\\DSA\\FlileZipper\\encoded_file.bin",ios::binary);
+    // Define a bitset that can hold sizeof(unsigned long) bits
+    bitset<100> bits(encoded_string);
+
+    unsigned long binary_value = bits.to_ulong();
+
+    // // write the binary value to file
+    myfile.write((const char*)&binary_value, m);
+    myfile.close();
+}
+
+int main(){
+    string encoded_string = "10101010010101010010";
+    getEncodedBinaryFile(encoded_string);
+
+    return 0;
+
+}
