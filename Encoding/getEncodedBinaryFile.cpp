2d00a1c3901dd4dd6235c704085e42311c2699aa
Annas03 <baigannas9@gmail.com>
2022-12-18 10:28:35 +0500

    update getEncodedBinaryFile.cpp

--- Encoding/getEncodedBinaryFile.cpp
+++ Encoding/getEncodedBinaryFile.cpp
@@ -1,13 +1,19 @@
 using namespace std;
 void getEncodedBinaryFile(string encoded_string){
-    // ofstream codedFile("codedFile.txt", ios::binary);
     ofstream myfile;
-    myfile.open("D:\\Annas Docs\\fourth sem\\DSA\\FlileZipper\\Encoding\\encoded_file.bin",ios::binary);
+    myfile.open("D:\\Annas Docs\\fourth sem\\DSA\\FlileZipper\\Encoding\\enc_file.bin",ios::binary);
     // Define a bitset that can hold sizeof(unsigned long) bits
-    bitset<sizeof(unsigned long) * 8> bits(encoded_string);
-
+    int i=1;
+    while(i*32 < encoded_string.length()){
+        bitset<32> bits(encoded_string.substr((i-1)*32, 32));
+        unsigned long binary_value = bits.to_ulong();
+        myfile.write((const char*)&binary_value, 4);
+        i++;
+    }
+    bitset<32> bits(encoded_string.substr((i-1)*32+1, encoded_string.length()-((i-1)*32+1)+1));
     unsigned long binary_value = bits.to_ulong();
+    myfile.write((const char*)&binary_value,  multiple(encoded_string.substr((i-1)*32+1, encoded_string.length()-((i-1)*32+1)+1)));
 
-    // write the binary value to file
-    myfile.write((const char*)&binary_value, sizeof(unsigned long));
+    // // write the binary value to file
+    myfile.close();
 }
